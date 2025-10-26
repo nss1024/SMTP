f334de3b6b8c18b84dd66fa248b587ddec0ccbdb
nss1024 <32052333+nss1024@users.noreply.github.com>
2025-10-26 18:43:42 +0000

    feat(smtp): add SessionState enum to represent SMTP session phases

---
+++ SMTPServer/src/utils/SessionState.java
@@ -0,0 +1,12 @@
+package utils;
+
+public enum SessionState {
+    INIT,                 // connection just established, waiting for HELO/EHLO
+    HELO_RECEIVED,        // HELO/EHLO successfully received
+    MAIL_FROM_RECEIVED,   // MAIL FROM command received
+    RCPT_TO_RECEIVED,     // at least one RCPT TO received
+    DATA_RECEIVING,       // after DATA command, reading message body
+    DATA_COMPLETE,        // end of DATA (line with just .)
+    QUIT                  // QUIT command received, session ending
+}
+
