90040c975d0025098066efcda39b9714355af0cd
nss1024 <32052333+nss1024@users.noreply.github.com>
2025-11-11 18:21:40 +0000

    feat: implement RealyEmail to save email and meta data to disk, started adding EmailMetaData class to hold forwarding information for future relay logic

---
+++ SMTPServer/src/mda/EmailMetaData.java
@@ -0,0 +1,13 @@
+package mda;
+
+public class EmailMetaData {
+
+    EmailMetaData(){
+        
+    }
+
+    public String toFileFormat(){
+        return "";
+    }
+
+}
---
+++ SMTPServer/src/mda/RelayEmail.java
@@ -0,0 +1,55 @@
+package mda;
+
+import commands.SMTPEmail;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RelayEmail implements Runnable{
+    private Logger logger = Logger.getLogger(this.getClass().getSimpleName());
+    private SMTPEmail smtpEmail;
+    private String fileName;
+    private String metaName;
+    private  String fileUudi;
+    private final String fileExtension = ".eml";
+    private final String metaExtension = ".meta";
+    private Path relayDirectory;
+    private Path metaDirectiry;
+    private  EmailMetaData emailMetaData;
+
+    RelayEmail(SMTPEmail email, EmailMetaData emailMetaData, Path relayDirectory, Path metaDirectiry){
+        this.smtpEmail=email;
+        this.relayDirectory=relayDirectory;
+        this.emailMetaData = emailMetaData;
+    }
+
+    @Override
+    public void run() {
+        fileUudi = UUID.randomUUID().toString();
+        fileName = fileUudi +fileExtension;
+        metaName = fileUudi + metaExtension;
+        Path fullPath = relayDirectory.resolve(fileName);
+        Path metaPath = metaDirectiry.resolve(metaName);
+
+        try{
+            Files.write(fullPath,smtpEmail.toEmlFormat().getBytes(StandardCharsets.UTF_8));
+
+
+        } catch (IOException e) {
+            logger.log(Level.WARNING,"Failed to save email to disk! "+e.getMessage());
+        }
+
+        try{
+
+            Files.write(metaPath,emailMetaData.toFileFormat().getBytes(StandardCharsets.UTF_8));
+
+        } catch (IOException e) {
+            logger.log(Level.WARNING,"Failed to save meta data to disk! "+e.getMessage());
+        }
+    }
+}
